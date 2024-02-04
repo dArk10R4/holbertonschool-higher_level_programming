bea2e4d22cafe38fcc98377082552eef45d634b1
dArk10R4 <goylerov04@gmail.com>
2024-02-04 11:53:31 -0800

    tasks

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -51,7 +51,7 @@ class Square:
             print("")
             return
         if self.__position[1] > 0:
-            for l in range(0, self.__position[1]):
+            for _ in range(0, self.__position[1]):
                 print("")
         for i in range(0, self.__size):
             if self.__position[0] > 0:
